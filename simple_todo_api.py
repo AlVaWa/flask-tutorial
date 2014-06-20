ac277b167d6e8f9c90485d49a15e1d0fd392553f
unknown <et34353@PC32006.corp.corpcommon.com>
2014-06-20 10:20:29 +0200

    our solution

--- simple_todo_api.py
+++ simple_todo_api.py
@@ -23,10 +23,12 @@ class Todo(db.Model):
 	text = db.Column('Text', db.String)
 	completed = db.Column('Completed', db.Boolean, default=False)
 	created = db.Column('Created', db.DateTime)
+	priority = db.Column('Priority', db.Integer, default=1)
 
 	def __init__(self, text=''):
 		self.text = text
 		self.completed = 0
+		priority = 1
 		self.created = datetime.now()
 
 	def to_dict(self):
@@ -34,7 +36,8 @@ class Todo(db.Model):
 			'id': self.id,
 			'text': self.text,
 			'completed': self.completed,
-			'created': self.created.isoformat()
+			'created': self.created.isoformat(),
+			'priority': self.priority
 		}
 
 
@@ -63,7 +66,7 @@ def index():
 	return flask.render_template('todos.html', title="Simple TODO")
 
 
-@app.route('/api/todos')
+@app.route('/api/todos', methods=['GET'])
 def api_get_todos():
 	todos = db.session.query(Todo).all()
 
@@ -73,6 +76,72 @@ def api_get_todos():
 	response.data = to_json(todos)
 
 	return response
+
+
+
+@app.route('/api/todos/<id>')
+def api_get_todo(id):
+	todo = db.session.query(Todo).get(id)
+
+	response = flask.make_response()
+	response.mimetype = 'application/json'
+	response.status_code = 200
+	response.data = to_json(todo)
+
+	return response
+
+@app.route('/api/todos', methods=['POST'])
+def api_create_todo():
+	payload = flask.request.get_json()
+	text = payload.get('text', 'Default text')
+	
+	todo = Todo(text)
+	db.session.add(todo)
+	db.session.commit()
+
+	response = flask.make_response()
+	response.mimetype = 'application/json'
+	response.status_code = 201
+	response.data = to_json(todo)
+
+	return response
+
+
+@app.route('/api/todos/<id>', methods=['DELETE'])
+def api_delete_todo(id):
+	todo = db.session.query(Todo).get(id)
+	db.session.delete(todo)
+	db.session.commit()
+
+	response = flask.make_response()
+	response.mimetype = 'application/json'
+	response.status_code = 200
+
+	return response
+
+@app.route('/api/todos/<id>', methods=['PUT'])
+def api_update_todo(id):
+	payload = flask.request.get_json()
+	
+	response = flask.make_response()
+
+	todo = db.session.query(Todo).get(id)
+	if todo is None:
+		response.status_code = 500
+	else:
+		text = payload.get('text', todo.text)
+		completed = payload.get('completed', todo.completed)
+		priority = payload.get('priority', todo.priority)
+		todo.text = text
+		todo.priority = priority
+		todo.completed = completed
+		db.session.commit()
+		response.status_code = 200
+		response.data = to_json(todo)	
+
+	response.mimetype = 'application/json'
+	return response
+
 
 
 # ===============================================
